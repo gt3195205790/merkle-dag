cfe9f2231dd5cad0995924d7f9f76f5749c0e247
陈炯颐 <3195205790@qq.com>
2024-03-12 18:02:14 +0800

    version_01

--- dag.go
+++ dag.go
@@ -3,6 +3,36 @@ package merkledag
 import "hash"
 
 func Add(store KVStore, node Node, h hash.Hash) []byte {
-	// TODO 将分片写入到KVStore中，并返回Merkle Root
+
+	if node.Type() == FILE {
+
+		fileNode := node.(File)
+		value := fileNode.Bytes()
+		err := store.Put(h.Sum(nil), value)
+		if err != nil {
+			return nil
+		}
+		return h.Sum(nil)
+	} else if node.Type() == DIR {
+
+		dirNode := node.(Dir)
+		it := dirNode.It()
+		var childHashes [][]byte
+		for it.Next() {
+			childNode := it.Node()
+			childHash := Add(store, childNode, h)
+			if childHash == nil {
+
+				return nil
+			}
+			childHashes = append(childHashes, childHash)
+		}
+
+		for _, hash := range childHashes {
+			h.Write(hash)
+		}
+		return h.Sum(nil)
+	}
+
 	return nil
 }
